821f3eac84e69c69d078a1cadc79c7d77f5e117c
keilander <kurt.eilander@emerson.com>
2026-09-22 08:41:21 -0500

    Yes, use the background threads by default

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ Settings for how k_runner works
 #
 # According to rumor, enabling daemon threads allows the
 # program to exit while child threads are still running.
-useDaemonThreads=False
+useDaemonThreads=True
